90ebbdbb721f3629c482cb2a6293e897f2ec0f44
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-01-02 14:53:02 +0545

    Add variable declarations and comments

--- variables.js
+++ variables.js
@@ -1,2 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello World";
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 3455;
+// userId.toFixed()
+// userId='heh'
+// boolean
+var isLoggedIn = false;
--- variables.ts
+++ variables.ts
@@ -3,4 +3,15 @@ const greetings: string = "Hello World";
 greetings.toLowerCase()
 
 console.log(greetings);
+
+// number
+let userId = 3455;
+// userId.toFixed()
+// userId='heh'
+
+// boolean
+
+const isLoggedIn : boolean = false;
+
+
 export{}
