983c9dafba8506336892a4f240595ef464ec9e13
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-01-04 20:04:57 +0545

    Add functions for adding two numbers and getting uppercase value

---
+++ function.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+}
+var loginUser = function (email, name, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+getUpper('asu');
+addTwo(5);
+signUpUser('asim', 'asimneupane11@gmail.com', false);
+loginUser('a', 'h@');
