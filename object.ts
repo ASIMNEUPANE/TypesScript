c12ace4b7cf7508ce34cfb9a01d8ee96ef8aab3f
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-01-04 20:05:10 +0545

    Add user and course creation functions

---
+++ object.ts
@@ -0,0 +1,20 @@
+const user = {
+name:"asim",
+email:"a@",
+isActive:true
+}
+
+function createUSer ({name:string, isActive:boolean}){
+
+}
+createUSer({name:'asim', isActive:false})
+function creasteCorse({name:string, isActive:boolean}){
+}
+
+
+
+let news =( {name:'asim', isActive:'false',email:'asim@'})
+
+creasteCorse(news)
+export{}
+
