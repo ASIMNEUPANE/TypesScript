fafcd71105bc9bb07f6ed43c012df1f160ceaf1c
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2024-01-05 01:20:07 +0545

    Fix commented out code and remove unused functions

--- object.ts
+++ object.ts
@@ -1,20 +1,40 @@
-const user = {
-name:"asim",
-email:"a@",
-isActive:true
-}
+// const user = {
+// name:"asim",
+// email:"a@",
+// isActive:true
+// }
 
-function createUSer ({name:string, isActive:boolean}){
+// function createUSer ({name:string, isActive:boolean}){
 
-}
-createUSer({name:'asim', isActive:false})
-function creasteCorse({name:string, isActive:boolean}){
-}
+// }
+// createUSer({name:'asim', isActive:false})
+// function creasteCorse({name:string, isActive:boolean}){
+// }
 
 
 
-let news =( {name:'asim', isActive:'false',email:'asim@'})
+// let news =( {name:'asim', isActive:'false',email:'asim@'})
 
-creasteCorse(news)
+// creasteCorse(news)
+
+
+
+// type User = {
+//     name:string;
+//     email:string;
+//     isActive:boolean;
+// }
+
+// function createUser(user:User):User{
+// return {name:'',email:'', isActive:false}
+// }
+
+// createUser({name:'',email:'', isActive:false})
+
+
+
+
+
+
 export{}
 
